fa6170c11249808e90f87b31269d292d357882ae
EMOTE-Project <emote.hwu@gmail.com>
2016-02-11 11:49:33 +0000

    WIP folder re-instated

---
+++ EMOTE-Modules-WIP/UtteranceManagerWPF/UtteranceManagerStandalone/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace UtteranceManagerClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
